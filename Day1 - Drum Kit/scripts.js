f183a88382a35d3f80aec1b91d7daf042ec9d2ac
NicolaFerracin <nicola.ferracin@gmail.com>
2016-12-09 23:56:37 +0100

    Add Day 1

---
+++ Day1 - Drum Kit/scripts.js
@@ -0,0 +1,62 @@
+(function() {
+  let startingTimestamp = 0;
+  let log = [];
+  let isReplaying = false;
+
+  function init() {
+    log = [];
+    isReplaying = false;
+  }
+
+  document.addEventListener('keydown', keydown);
+
+  const drumItems = document.querySelectorAll('.drum-item');
+  drumItems.forEach(item => item.addEventListener('transitionend', transitionend));
+
+  function keydown(e) {
+    const keyCode = e.keyCode;
+    playKey(keyCode);
+  }
+
+  function playKey(key) {
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    const drumItem = document.querySelector(`[data-key="${key}"]`);
+    if (audio && drumItem) {
+      if (!isReplaying) {
+        addLogEntry(key);
+      }
+      audio.currentTime = 0;
+      audio.play();
+      drumItem.classList.add('active');
+    }
+  }
+
+  function transitionend(e) {
+    this.classList.remove('active');
+  }
+
+  function addLogEntry(key) {
+    let newEntry = {};
+    if (log.length === 0) {
+      newEntry = {timestamp: 0, key: key};
+      startingTimestamp = new Date().getTime();
+    } else {
+      newEntry = {timestamp: new Date().getTime() - startingTimestamp, key: key};
+    }
+    log.push(newEntry);
+  }
+
+  document.replay = () => {
+    if (log.length > 0) {
+      isReplaying = true;
+    }
+    log.forEach((entry, index) => {
+      setTimeout(() => {
+        playKey(entry.key);
+        if (index === log.length - 1) {
+          init();
+        }
+      }, entry.timestamp)
+    });
+  }
+}())
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-text
+Repo collecting all the excercices/challenges from Wes Bos "[30 Day Vanilla JS Coding Challenge](https://javascript30.com/)".
+
+Goal of this series of challenges is to get rid of every possible overhead given by libraries and framemorks and use only "pure" vanilla JavaScript.
+
+
+# Day 1 - Drum Kit
+Simple drum kit that can be played on the keaboard.
+
+### Bonus
+Replay button that plays back the last sequence of keyboard/drum strokes, with correct timing between each stroke.
