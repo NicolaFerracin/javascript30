95682bd6f1652f4c78410ad33031951c2664190b
NicolaFerracin <nicola.ferracin@gmail.com>
2016-12-31 13:46:42 +0100

    Add day 9 challenge

---
+++ Day9 - Shift multiple selection/scripts.js
@@ -0,0 +1,19 @@
+(function() {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  let lastSelected = null;
+
+  checkboxes.forEach(checkbox => checkbox.addEventListener('click', function(e) {
+    if (e.shiftKey && this.checked) {
+      if (lastSelected < this.dataset.position) {
+        for (let i = lastSelected; i < this.dataset.position; i++) {
+          checkboxes[i].checked = true;
+        }
+      } else {
+        for (let i = this.dataset.position; i < lastSelected; i++) {
+          checkboxes[i].checked = true;
+        }
+      }
+    }
+    lastSelected = this.dataset.position;
+  }));
+}());
--- README.md
+++ README.md
@@ -35,3 +35,7 @@ Create an interactive image gallery using flxebox.
 
 # Day 6 - Ajax and Search Filter
 Use fetch() to get a list of 1000 cities in the US and add a search by City or State funcionality.
+---
+
+# Day 9 - Shift multiple selection
+Allow multiple selection of checkboxes while holding the shift-key down.
