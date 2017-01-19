78eecd30a5db8312952c205511e2121ce3e14ce7
NicolaFerracin <nicola.ferracin@gmail.com>
2017-01-19 19:37:42 +0100

    Day1 - add button to show .js file and fix bug on replay

--- Day1 - Drum Kit/scripts.js
+++ Day1 - Drum Kit/scripts.js
@@ -1,52 +1,55 @@
-(function() {
-  let startingTimestamp = 0;
-  let log = [];
-  let isReplaying = false;
+let startingTimestamp = 0;
+let log = [];
+let previousLog = [];
+let isReplaying;
+init();
+document.addEventListener('keydown', keydown);
+const drumItems = document.querySelectorAll('.drum-item');
+drumItems.forEach(item => item.addEventListener('transitionend', transitionend));
 
-  function init() {
-    log = [];
-    isReplaying = false;
+function init() {
+  if (log.length > 0) {
+    previousLog = log.slice(0);
   }
+  log = [];
+  isReplaying = false;
+}
 
-  document.addEventListener('keydown', keydown);
+function keydown(e) {
+  const keyCode = e.keyCode;
+  playKey(keyCode);
+}
 
-  const drumItems = document.querySelectorAll('.drum-item');
-  drumItems.forEach(item => item.addEventListener('transitionend', transitionend));
-
-  function keydown(e) {
-    const keyCode = e.keyCode;
-    playKey(keyCode);
-  }
-
-  function playKey(key) {
-    const audio = document.querySelector(`audio[data-key="${key}"]`);
-    const drumItem = document.querySelector(`[data-key="${key}"]`);
-    if (audio && drumItem) {
-      if (!isReplaying) {
-        addLogEntry(key);
-      }
-      audio.currentTime = 0;
-      audio.play();
-      drumItem.classList.add('active');
+function playKey(key) {
+  const audio = document.querySelector(`audio[data-key="${key}"]`);
+  const drumItem = document.querySelector(`[data-key="${key}"]`);
+  if (audio && drumItem) {
+    if (!isReplaying) {
+      addLogEntry(key);
     }
+    audio.currentTime = 0;
+    audio.play();
+    drumItem.classList.add('active');
   }
+}
 
-  function transitionend() {
-    this.classList.remove('active');
+function transitionend() {
+  this.classList.remove('active');
+}
+
+function addLogEntry(key) {
+  let newEntry = {};
+  if (log.length === 0) {
+    newEntry = {timestamp: 0, key: key};
+    startingTimestamp = new Date().getTime();
+  } else {
+    newEntry = {timestamp: new Date().getTime() - startingTimestamp, key: key};
   }
+  log.push(newEntry);
+}
 
-  function addLogEntry(key) {
-    let newEntry = {};
-    if (log.length === 0) {
-      newEntry = {timestamp: 0, key: key};
-      startingTimestamp = new Date().getTime();
-    } else {
-      newEntry = {timestamp: new Date().getTime() - startingTimestamp, key: key};
-    }
-    log.push(newEntry);
-  }
-
-  document.replay = () => {
+function replay(log) {
+  if (!isReplaying) {
     isReplaying = true;
     log.forEach((entry, index) => {
       setTimeout(() => {
@@ -57,4 +60,12 @@
       }, entry.timestamp)
     });
   }
-}())
+}
+
+document.replay = () => {
+  if (log.length === 0 && previousLog.length > 0) {
+    replay(previousLog);
+  } else {
+    replay(log);
+  }
+}
